f4f31ac050633d992059bdb67809791f14306a6a
RenatoAguirre <raaguirre1@miuandes.cl>
2024-09-29 18:17:20 -0300

    feat(backend): handle event picture attachment

--- backend/app/controllers/api/v1/event_pictures_controller.rb
+++ backend/app/controllers/api/v1/event_pictures_controller.rb
@@ -1,31 +1,20 @@
 class API::V1::EventPicturesController < ApplicationController
   include Authenticable
+  include ImageProcessing  # Assuming you have a module to process Base64 images
   before_action :verify_jwt_token
   before_action :set_event
   before_action :set_event_picture, only: [:destroy]
 
-  def index
-    @event_pictures = @event.event_pictures
-    render json: @event_pictures.as_json, status: :ok
-  end
-
   def create
     @event_picture = @event.event_pictures.new(event_picture_params)
-    @event_picture.user_id = current_user.id  # Asignar el usuario autenticado
+    @event_picture.user_id = current_user.id  # Assign the current user as the uploader
+
+    handle_image_attachment if event_picture_params[:image_base64]
 
     if @event_picture.save
       render json: { message: 'Image successfully uploaded.', event_picture: @event_picture }, status: :created
     else
       render json: @event_picture.errors, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    if @event_picture.user_id == current_user.id
-      @event_picture.destroy
-      render json: { message: 'Image successfully deleted.' }, status: :no_content
-    else
-      render json: { error: 'You are not authorized to delete this image.' }, status: :forbidden
     end
   end
 
@@ -36,12 +25,18 @@ class API::V1::EventPicturesController < ApplicationController
     render json: { error: 'Event not found' }, status: :not_found unless @event
   end
 
+  def event_picture_params
+    params.require(:event_picture).permit(:description, :image_base64)
+  end
+
+  # Adapted handle_image_attachment for EventPicture
+  def handle_image_attachment
+    decoded_image = decode_image(event_picture_params[:image_base64])
+    @event_picture.image.attach(io: decoded_image[:io], filename: decoded_image[:filename], content_type: decoded_image[:content_type])
+  end
+
   def set_event_picture
     @event_picture = @event.event_pictures.find_by(id: params[:id])
     render json: { error: 'Image not found' }, status: :not_found unless @event_picture
-  end
-
-  def event_picture_params
-    params.require(:event_picture).permit(:description)
   end
 end
